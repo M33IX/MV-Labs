496f9dec986708f6a78a904f2b3345fa58cc5142
Andrew <sgtsh4ksw@gmail.com>
2024-06-02 17:10:31 +0300

    feat: Make functions prototypes

--- mv4.py
+++ mv4.py
@@ -8,16 +8,31 @@ def f(x: float, y: float) -> float:
 
 def AnalyticSolution(x: float) -> float:
     """Возращает точное аналитическое решение дифференциального 
-    уравнения
-    """
+    уравнения"""
     return 0.0
 
-def TureValue(x: float, n: int)-> list:
+def TureValue(x: list[float], n: int)-> list[float]:
     """Возвращает точное значение в заданых точках x"""
-    return 1
+    return list(0)
 
-def CalculateEuler(x: float, y: float, n: int, h: float) -> list:
+def CalculateEuler(x: list[float], y: list[float], n: int, h: float) -> list[float]:
     """Возвращает решение численного дифференциального 
-    уравнения по методу Эйлера
-    """
-    return 0.0
+    уравнения по методу Эйлера"""
+    return list(0)
+
+def CalculateEulerModified(x: list[float], y: list[float], n: int, h: float) -> list[float]:
+    """Производит вычисление по модифицированному
+    методу Эйлера"""
+    return list(0)
+
+def CalculateRungeKutta(x: list[float], y: list[float], n: int, h: float) -> list[float]:
+    """Производит вычисление по методы Рунге-Кутты"""
+    return list(0)
+
+def MakeTable() -> None:
+    """Мне уже"""
+    pass
+
+def CalculateError() -> None:
+    """Просто похуй"""
+    pass
