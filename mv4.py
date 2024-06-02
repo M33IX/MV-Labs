c814612c13043f28ad1cc516b623c4431e1e3e68
Andrew <sgtsh4ksw@gmail.com>
2024-06-02 18:29:24 +0300

    feat: Описать мат функции и метод-обертку над ними

--- mv4.py
+++ mv4.py
@@ -11,23 +11,45 @@ def AnalyticSolution(x: float) -> float:
     уравнения"""
     return 0.0
 
-def TureValue(x: list[float], n: int)-> list[float]:
+def TureValue(x: list[float], n: float)-> list[float]:
     """Возвращает точное значение в заданых точках x"""
-    return list(0)
+    y = list()
+    for i in range(1, n+1):
+        y.append(AnalyticSolution(x[i]))
+    return y
 
-def CalculateEuler(x: list[float], y: list[float], n: int, h: float) -> list[float]:
-    """Возвращает решение численного дифференциального 
-    уравнения по методу Эйлера"""
-    return list(0)
+def CalculateIterations(method:function, x: list[float], y0: float, n: float, h: float) -> list[float]:
+    y = [y0]
+    hf = list()
+    for i in range(0, int(n)):
+        result = method(x[i], y[i], h)
+        hf.append(result[0])
+        y.append(result[1])
+    hf.append(0)
+    MakeTable()
+    return y
 
-def CalculateEulerModified(x: list[float], y: list[float], n: int, h: float) -> list[float]:
-    """Производит вычисление по модифицированному
-    методу Эйлера"""
-    return list(0)
+def EulersMethod(x: float, y: float, h: float) -> tuple[float, float]:
+    """Считает значение прироста и значение y на одной итерации"""
+    hf = h * f(x,y)
+    y += hf
+    return (hf, y)
 
-def CalculateRungeKutta(x: list[float], y: list[float], n: int, h: float) -> list[float]:
-    """Производит вычисление по методы Рунге-Кутты"""
-    return list(0)
+def ModifiedEulersMethod(x: float, y: float, h: float) -> tuple[float, float]:
+    """Считает значение прироста и значение y на одной итерации"""
+    y2 = (h / 2) * f(x,y)
+    hf = h * f(x + (h / 2), y + y2)
+    y += hf
+    return (hf, y)
+
+def RungeKuttasMethod(x: float, y: float, h: float) -> tuple[float, float]:
+    """Считает значение прироста и значение y на одной итерации"""
+    k1 = h * f(x, y)
+    k2 = h * f(x + ( h / 2 ), y + (k1 / 2))
+    k3 = h * f(x + h, y + 2 * k2 - k1)
+    hf = (1 / 6) * (k1 + 4 * k2 + k3)
+    y += hf
+    return (hf, y)
 
 def MakeTable() -> None:
     """Мне уже"""
